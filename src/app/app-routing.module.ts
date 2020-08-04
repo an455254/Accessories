66e7dd23c4b664b4625c725ed09b65e431071b63
Anudish <an455254@dal.ca>
2020-08-03 23:41:12 -0300

    Accessories Front-End Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GetAccessoriesComponent } from './get-accessories/get-accessories.component';
+import { HandleAccessoriesComponent } from './handle-accessories/handle-accessories.component';
+import {DisplayordersComponent} from './displayorders/displayorders.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GetAccessoriesComponent , pathMatch: 'full' },
+  { path: 'getAccessories', component: GetAccessoriesComponent},
+  { path: 'handleAccssory', component: HandleAccessoriesComponent},
+  { path: 'displayorders', component: DisplayordersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,58 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { GetAccessoriesComponent } from './get-accessories/get-accessories.component';
+import { HandleAccessoriesComponent } from './handle-accessories/handle-accessories.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSnackBarModule} from "@angular/material/snack-bar";
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { from } from 'rxjs';
+import { HeaderComponent } from './header/header.component';
+import { DisplayordersComponent } from './displayorders/displayorders.component';
+import { AccessoryfilterPipe } from './get-accessories/accessoryfilter.pipe';
+import { OrdersfilterPipe } from './displayorders/ordersfilter.pipe';
+import { FooterComponent } from './footer/footer.component';
+import { AccessoriesDbInteractionService} from './accessories-db-interaction.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetAccessoriesComponent,
+    HandleAccessoriesComponent,
+    HeaderComponent,
+    DisplayordersComponent,
+    AccessoryfilterPipe,
+    OrdersfilterPipe,
+    FooterComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatCardModule,
+    MatDialogModule,
+    HttpClientModule,
+    MatSlideToggleModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [AccessoriesDbInteractionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
